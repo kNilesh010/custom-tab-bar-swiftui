d6a17ff83f44b2b544bc570f3d56e47ea7237c5f
Nilesh Kumar <k.nilesh010@gmail.com>
2022-06-11 22:46:27 +0530

    Initial Commit

---
+++ Custom-Tab-Bar-SwiftUI/Custom_Tab_Bar_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_Tab_Bar_SwiftUIApp.swift
+//  Custom-Tab-Bar-SwiftUI
+//
+//  Created by Nilesh Kumar on 11/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Custom_Tab_Bar_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
